5245c5f42bc7cfeb1aee682a1858a317be6b6264
akilinjamam <akilinjamam@gmail.comm>
2022-05-01 23:50:58 +0600

    mongodb-connected-with-server

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
 const express = require('express')
 const app = express();
 const port = process.env.PORT || 5000
@@ -19,3 +21,31 @@ app.get('/', (req, res) => {
     res.send('running my node crud server')
 })
 
+
+
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.8ajdo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    await client.connect()
+    const productsCollection = client.db('exertion').collection('products')
+
+    try {
+        app.get('/products', async (req, res) => {
+            const query = {}
+            const cursor = productsCollection.find(query)
+            const product = await cursor.toArray()
+            res.send(product)
+        })
+
+    }
+    finally {
+
+    }
+}
+
+run().catch(console.dir)
